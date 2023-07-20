40af9be6dc69c75b9c9d4f40452909a5689bb4da
Husted42 <alx2608@gmail.com>
2023-07-20 09:55:02 +0200

    Update numpy00.py

--- pre/exercises/numpy00.py
+++ pre/exercises/numpy00.py
@@ -9,14 +9,27 @@ arr3d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
 arr5d = np.array([1,2,3,4], ndmin=5)
 
 ##### ----- Functions ----- #####
-def copyView(inp):
-    
+def fun_copy():
+    arr = np.array([1,2,3,4,5])
+    x = arr.copy()
+    arr[0] = 42
+    print('copy not effected:')
+    print('(original, copy) : ', (arr, x))
+
+def fun_view():
+    arr = np.array([1,2,3,4,5])
+    x = arr.view()
+    arr[0] = 42
+    print('copy is effected:')
+    print('(original, copy) : ', (arr, x))
+
 
 ##### ----- Calls ----- #####
 #Dimension in Arrays
 print(' -- Dimension in Arrays --')
 print(arr0d.ndim, arr1d.ndim, arr2d.ndim, arr3d.ndim)
-print('number of dimensions :', arr5d.ndim, arr5d, '\n\n')
+print('number of dimensions : ', arr5d.ndim, arr5d, )
+print('shape: ', arr1d.shape, arr2d.shape, arr3d.shape, arr5d.shape, '\n\n')
 
 #Access Array Elements
 print(' -- Access Array Elements --')
@@ -27,7 +40,11 @@ print('positive slicing : ', arr1d[2:4])
 print('negative slicing : ', arr1d[-4:-2])
 print('step slicing (every other element): ', arr1d[0:3:2])
 print('step slicing (every other element): ', arr1d[::2])
-print('from both elements, return index 2: ', arr2d[0:2, 2])
+print('from both elements, return index 2: ', arr2d[0:2, 2], '\n\n')
 
 #Access Array Elements
-copyView(arr1d)
+fun_copy()
+fun_view()
+print('\n\n')
+
+#Reshape
